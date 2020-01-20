a661405d44a3d601604e977f70a64820d9f03a56
fordpelenio2 <59575734+fordpelenio2@users.noreply.github.com>
2020-01-20 13:55:39 -0500

    Add files via upload

---
+++ week-03/index.js
@@ -0,0 +1,32 @@
+console.log('it works');
+
+let animal = 'cat';
+
+let phone = 6479660103;
+
+let itIsmonday = false;
+
+console.log(animal,phone,itIsmonday);
+
+
+if (typeof animal === 'cat') {
+    console.log('the animal is a cat')
+}
+
+if (typeof phone === 'number') {
+    console.log('phone number type')
+}
+
+if (typeof itIsmonday === "boolean") {
+    console.log('it is boolean')
+}
+
+let comp1231;
+let classTime = Math.floor(Math.random()* 100);
+
+
+
+if (number <=25) {
+    console.log('the number is', number)
+} else if (number> 25 && number <= 50) {
+    console.log('the number is', number);
